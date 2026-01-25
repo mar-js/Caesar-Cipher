dd09a41f912ba35666fc91e8830a35d1f37a261f
Martin <maguera37@gmail.com>
2026-01-25 17:06:08 -0300

    feat: added Utils module with ALPHABET constant

---
+++ utils/constants.rb
@@ -0,0 +1,9 @@
+module Utils
+  ALPHABET = [
+    "a", "b", "c", "d", "e", "f",
+    "g", "h", "i", "j", "q", "l",
+    "ll", "m", "n", "o", "p", "q",
+    "r", "s", "t", "u", "v", "w",
+    "x", "y", "z"
+  ]
+end
