a8e7643f312b60bb20b8fd8020f89ae9d2adb294
Martin <maguera37@gmail.com>
2026-02-01 17:16:06 -0300

    feat: simplied the ALPHABET constant using a range

--- utils/constants.rb
+++ utils/constants.rb
@@ -1,9 +1,3 @@
 module Utils
-  ALPHABET = [
-    "a", "b", "c", "d", "e", "f",
-    "g", "h", "i", "j", "q", "l",
-    "ll", "m", "n", "o", "p", "q",
-    "r", "s", "t", "u", "v", "w",
-    "x", "y", "z"
-  ]
+  ALPHABET = ("a"..."z").to_a
 end
